d1423a6f88560602d47c148bae6cfce521fdf30f
johnssonmi <113111853+johnssonmi@users.noreply.github.com>
2023-12-18 12:09:23 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,40 +1,14 @@
 // //skapar en variabel url, som innehåller den adress som anropet ska göras till
 const url = 'http://localhost:3000/users';
 
-// // argument till fetch()-anropet response i den anonyma callbackfunktionen skickas till then()-funktionen,
-// //och översätts från ett response-objekt till JSON genom att anropa funktionen json()
-// fetch(url)
-//   .then((response) => {
-//     console.log(response);
-//     return response.json();
-//   })
-//   //Använder återigen then() för att få output
-//   .then((jsonData) => console.log(jsonData));
 
-// // Skapa en async-funktion för att hämta och bearbeta data
-// async function fetchData(url) {
-//   try {
-//     const response = await fetch(url); // Vänta på att hämta data från den angivna URL:en
-//     const users = await response.json(); // Vänta på att omvandla responsen till JSON-format
-//     users.forEach((user) => {
-//       // Loopa igenom varje användare och skapa HTML för varje
-//       const html = `<p>${user.id} ${user.firstName} ${user.lastName} ${user.username} ${user.color}</p>`;
-
-//       document.body.insertAdjacentHTML('beforeend', html); // Lägg till den genererade HTML-koden till slutet av dokumentet
-//     });
-//   } catch (error) {
-//     console.error('Error fetching data:', error); // Om det uppstår ett fel, logga det till konsolen
-//   }
-// }
-// fetchData(url); // Anropa funktionen för att hämta och bearbeta data från API:et
-
-// sorterad version 1-10
 // Skapa en async-funktion för att hämta och bearbeta data
 async function fetchData(url) {
   try {
     const response = await fetch(url); // Vänta på att hämta data från den angivna URL:en
     const users = await response.json(); // Vänta på att omvandla responsen till JSON-format
 
+    // Printa arrayen med users i console.log
     console.log(users)
 
     // Skapa en ul-container för användarlistan
